9b8d0e50e9cb6d69aa553e76b2dd825d6332cf34
gpaul <gregory.paul.ext@mappy.com>
2015-02-09 16:41:10 +0100

    After today’s coding dojo

--- app/src/RomanToNumeric.js
+++ app/src/RomanToNumeric.js
@@ -3,6 +3,24 @@
 function RomanToNumeric() {
 };
 
-RomanToNumeric.prototype.parse = function() {
-	return 0;
+RomanToNumeric.prototype.parse = function(romanNumber) {
+    var c = 0,
+	numbers = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500,
+		M: 1000
+	};
+	
+    for(var i = 0; i < romanNumber.length; i++) {
+       	var current = numbers[romanNumber.charAt(i)];
+       	var next = numbers[romanNumber.charAt(i+1)] || 0;
+
+	c += (current < next) ? -current : +current;
+    }
+
+    return c;
 };
--- test/spec/RomanToNumericTest.js
+++ test/spec/RomanToNumericTest.js
@@ -8,7 +8,75 @@ describe('RomanToNumeric', function() {
 	});
 
 	it('should parse I', function() {
-		expect(r2n.parse('I')).to.be.a('number').to.be.equal(1);
+		expect(r2n.parse('I')).to.be.a('number').to.equal(1);
 	});
+
+	it('should parse II', function() {
+		expect(r2n.parse('II')).to.equal(2);
+	});
+
+    it('should parse III', function() {
+		expect(r2n.parse('III')).to.equal(3);
+	});
+
+	it('should parse IV', function() {
+		expect(r2n.parse('IV')).to.equal(4);
+	});
+
+	it('should parse V', function() {
+		expect(r2n.parse('V')).to.equal(5);
+	});
+
+	it('should parse VI', function() {
+		expect(r2n.parse('VI')).to.equal(6);
+	});
+
+        it('should parse IX', function () {
+            expect(r2n.parse('IX')).to.equal(9);
+        });
+
+        it('should parse X', function () {
+            expect(r2n.parse('X')).to.equal(10);
+        });
+
+        it('should parse XI', function () {
+            expect(r2n.parse('XI')).to.equal(11);
+        });
+
+        it('should parse XLIX', function () {
+            expect(r2n.parse('XLIX')).to.equal(49);
+        });
+
+        it('should parse XLIV', function () {
+            expect(r2n.parse('XLIV')).to.equal(44);
+        });
+
+        it('should parse L', function () {
+            expect(r2n.parse('L')).to.equal(50);
+        });
+
+        it('should parse XL', function () {
+            expect(r2n.parse('XL')).to.equal(40);
+        });
+
+        it('should parse C', function () {
+            expect(r2n.parse('C')).to.equal(100);
+        });
+
+        it('should parse CD', function () {
+            expect(r2n.parse('CD')).to.equal(400);
+        });
+
+        it('should parse XC', function () {
+            expect(r2n.parse('XC')).to.equal(90);
+        });
+
+        it('should parse D', function () {
+            expect(r2n.parse('D')).to.equal(500);
+        });
+
+        it('should parse M', function () {
+            expect(r2n.parse('M')).to.equal(1000);
+        });
 
 });
